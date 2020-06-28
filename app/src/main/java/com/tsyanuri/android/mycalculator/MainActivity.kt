c5b1b31ac4b3f326aa29dc5e22e48afda5eb8310
tsyanuri <tsyanuri@gmail.com>
2020-06-28 23:52:55 +0700

    Initial commit

---
+++ app/src/main/java/com/tsyanuri/android/mycalculator/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.tsyanuri.android.mycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var lastNumeric : Boolean= false
+    var lastDot : Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View){
+       tvInput.append((view as Button).text)
+        lastNumeric = true
+    }
+
+    fun onClear(view: View){
+        tvInput.text = ""
+        lastNumeric =false
+        lastDot=false
+    }
+
+    fun onDecimalPoint(view: View){
+        if (lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+}
