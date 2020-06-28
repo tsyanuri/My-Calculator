de4c1849496e1cb1f0784d758b9914a25455d769
tsyanuri <tsyanuri@gmail.com>
2020-06-29 00:13:42 +0700

    Adding The onOperatorFunctionality Using Contains And startsWith On Strings

--- app/src/main/java/com/tsyanuri/android/mycalculator/MainActivity.kt
+++ app/src/main/java/com/tsyanuri/android/mycalculator/MainActivity.kt
@@ -9,30 +9,47 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var lastNumeric : Boolean= false
-    var lastDot : Boolean = false
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun onDigit(view: View){
-       tvInput.append((view as Button).text)
+    fun onDigit(view: View) {
+        tvInput.append((view as Button).text)
         lastNumeric = true
     }
 
-    fun onClear(view: View){
+    fun onClear(view: View) {
         tvInput.text = ""
-        lastNumeric =false
-        lastDot=false
+        lastNumeric = false
+        lastDot = false
     }
 
-    fun onDecimalPoint(view: View){
-        if (lastNumeric && !lastDot){
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
             tvInput.append(".")
             lastNumeric = false
             lastDot = true
         }
     }
+
+    fun onOperator(view: View) {
+        if (lastNumeric && !isOperatorAdded(tvInput.text.toString()))
+            tvInput.append((view as Button).text)
+        lastNumeric = false
+        lastDot = false
+    }
 }
+
+    private fun isOperatorAdded(value : String) : Boolean{
+        return if (value.startsWith("-")){
+            false
+        }else{
+            value.contains("÷") || value.contains("+") ||
+                    value.contains("-") || value.contains("×")
+        }
+    }
+
